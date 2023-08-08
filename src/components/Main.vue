eb57d57b15f9d8efe0545369dc774a3620b0ab2c
Kenley <wangql@rynnova.com>
2023-08-08 23:45:04 +0800

    添加主体组件

---
+++ src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main-com">
+    <MainBody></MainBody>
+    <DetailedInformation></DetailedInformation>
+  </div>
+</template>
+
+<script>
+import MainBody from './MainBody.vue'
+import DetailedInformation from './DetailedInformation.vue'
+export default {
+  name: "MainCom",
+  components: {
+    MainBody,
+    DetailedInformation
+  },
+};
+</script>
+
+<style scoped></style>
