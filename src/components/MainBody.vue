92d470c373429500341dbe0055d7352da05e0200
Kenley <wangql@rynnova.com>
2023-08-08 23:45:21 +0800

    添加主体头部组件

---
+++ src/components/MainBody.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main-body">d
+    <!-- <vs-button gradient :active="active == 0" @click="active = 0">
+      Active
+    </vs-button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainBody",
+  props: {
+    // msg: String
+  },
+//   data: () => ({
+//     active: 0,
+//   }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-body {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
